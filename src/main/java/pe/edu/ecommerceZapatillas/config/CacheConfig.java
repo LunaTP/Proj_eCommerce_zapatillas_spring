36dce242a8254a7fe4254bb806c5f4f0c2acda71
Anthony Christian Ramos Rojas <116607676+Tomoe127@users.noreply.github.com>
2024-12-07 17:11:47 -0500

    creacion de archivo cache (#3)

---
+++ src/main/java/pe/edu/ecommerceZapatillas/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package pe.edu.ecommerceZapatillas.config;
+
+import jakarta.persistence.Embeddable;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Embeddable
+public class CacheConfig {
+}
