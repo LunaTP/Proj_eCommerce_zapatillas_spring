0a28b7cf8092ea215f9d5e8755beb62c92eb5eea
Diego Alejandro Luna Tarapa <diegolt2014@gmail.com>
2024-12-07 15:41:08 -0500

    inicialización proyecto

---
+++ src/main/java/pe/edu/ecommerceZapatillas/EcommerceZapatillasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.ecommerceZapatillas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceZapatillasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceZapatillasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/ecommerceZapatillas/EcommerceZapatillasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.ecommerceZapatillas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceZapatillasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
